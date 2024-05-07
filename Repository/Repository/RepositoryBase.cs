f0c6d20c658d46268cfbe4d20e15e118ea1bb36e
malwamaba@gmail.com <malwamaba@gmail.com>
2024-05-07 21:06:52 +0200

    Modified the Repository project.

--- Repository/Repository/RepositoryBase.cs
+++ Repository/Repository/RepositoryBase.cs
@@ -1,12 +1,6 @@
 ﻿using Contracts;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository
 {
