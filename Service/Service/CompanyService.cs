d8d04863c8cebae0d6f921dd653e8e5334bec85a
malwamaba@gmail.com <malwamaba@gmail.com>
2024-06-09 20:25:25 +0200

    Added DeleteCompany and UpdateComnpany to Company Services.

--- Service/Service/CompanyService.cs
+++ Service/Service/CompanyService.cs
@@ -4,6 +4,7 @@ using Entities.Exceptions;
 using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
+using System.ComponentModel.Design;
 
 
 namespace Service
@@ -84,6 +85,26 @@ namespace Service
             var ids = string.Join(",", companyCollectionToReturn.Select(_ => _.Id));
 
             return (companies: companyCollectionToReturn, ids: ids);
+        }
+
+        public void DeleteCompany(Guid companyId, bool trackChanges) 
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            _repository.Company.DeleteCompany(company);
+            _repository.Save();
+        }
+
+        public void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges)
+        {
+            var companyEntity = _repository.Company.GetCompany(companyId, trackChanges);
+            if (companyEntity is null)
+                throw new CompanyNotFoundException(companyId);
+
+            _mapper.Map(companyForUpdate, companyEntity);
+            _repository.Save();
         }
     }
 }
